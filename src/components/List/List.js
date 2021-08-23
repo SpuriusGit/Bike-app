91ecea92b4dff1083ed2df7ff0c8ec38433cd526
unknown <d.tatochenko@gmail.com>
2021-08-23 20:24:21 +0300

    qqq

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListItem from '../ListItem/ListItem';
 
 import './list-style.css';
 
 const List = ({items,remove}) =>{
+    let [bikeStatus,setBikeStatus] = useState();
+    function checkBikeStatus(selectValue){
+        setBikeStatus(bikeStatus=selectValue);
+    }
     return (
         <div className="list">
             {items.map((item,key) =>
@@ -17,6 +21,8 @@ const List = ({items,remove}) =>{
                 id={item.id}   
                 remove={remove} 
                 item={item}
+                bikeStatus={bikeStatus}
+                checkBike={checkBikeStatus}
             />
             )}
         </div>
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -2,19 +2,20 @@ import React, { useState } from 'react';
 
 import './list-item-style.css';
 
-const ListItem = (props) =>{
+const ListItem = ({checkBike,type,color,name,id,item,price,remove}) =>{
     let [select,setSelect] = useState();
     let [selectValue,setValue] = useState();
 
     const change = (event) =>{
         setSelect(select = `list_item ${event.target.value}`);
         setValue(selectValue = event.target.value);
+        checkBike(selectValue);
     }
     return (
-        <div className={'list-item '+ select}>
+        <div className={'list-item '+ select} id={selectValue}>
             <div className="list-item__info">
-                <p><strong>{props.name}</strong> - {props.type}({props.color})</p>
-                <p className="list-item__id">id:{props.id}</p>
+                <p><strong>{name}</strong> - {type}({color})</p>
+                <p className="list-item__id">id:{id}</p>
                 <p className="list-item__status">
                     STATUS: <select className="list-item__select" onChange={change} value={selectValue}>
                                 <option value="Available">Available</option>
@@ -24,8 +25,8 @@ const ListItem = (props) =>{
                 </p>
             </div>
             <div className="list-item__right">
-                <button className="remove" onClick={()=> props.remove(props.item)}>Х</button>
-                <div className="list-item__price">{props.price}.00 UAH/hr.</div>
+                <button className="remove" onClick={()=> remove(item)}>Х</button>
+                <div className="list-item__price">{price}.00 UAH/hr.</div>
             </div>
         </div>
     );
